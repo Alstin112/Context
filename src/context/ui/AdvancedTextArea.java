4179ac777a86b2d0baa1328189b50ad56db00619
Alstin112 <alex.dasilvare@gmail.com>
2024-08-16 21:06:17 -0300

    Added functionality when typed TAB.

--- src/context/ui/AdvancedTextArea.java
+++ src/context/ui/AdvancedTextArea.java
@@ -1,5 +1,6 @@
 package context.ui;
 
+import arc.input.KeyCode;
 import arc.scene.Element;
 import arc.scene.event.InputEvent;
 import arc.scene.event.InputListener;
@@ -7,28 +8,21 @@ import arc.scene.ui.TextArea;
 
 public class AdvancedTextArea extends TextArea {
 
-    public AdvancedTextArea(String text) {
-        super(text);
+    public AdvancedTextArea(String newText) {
+        super(newText);
+        focusTraversal = false;
+        onlyFontChars = false;
 
-        addCaptureListener(new InputListener(){
+        addCaptureListener(new InputListener() {
             @Override
             public void enter(InputEvent event, float x, float y, int pointer, Element fromActor) {
                 requestScroll();
             }
         });
-        addListener(new InputListener(){
-            @Override
-            public boolean scrolled(InputEvent event, float x, float y, float amountX, float amountY) {
-                scroll((int) amountY);
-                return true;
-            }
-
-
-        });
     }
 
     public void scroll(int lines) {
-        firstLineShowing = Math.max(Math.min(firstLineShowing + lines, linesBreak.size/2-1),0);
+        firstLineShowing = Math.max(Math.min(firstLineShowing + lines, linesBreak.size / 2 - 1), 0);
     }
 
     @Override
@@ -37,7 +31,60 @@ public class AdvancedTextArea extends TextArea {
     }
 
     public class AdvancedTextAreaListener extends TextAreaListener {
+        @Override
+        public boolean keyDown(InputEvent event, KeyCode keycode) {
+            if (keycode == KeyCode.tab && hasSelection) {
+                int start = Math.min(cursor, selectionStart);
+                int end = Math.max(cursor, selectionStart);
+                boolean deleteLastChar = true;
 
+                String oldText = getText();
+                StringBuilder sb = new StringBuilder();
+                int totalAdded = 0;
+                for (int index = 0; index < linesBreak.size; index += 2) {
+                    if (linesBreak.get(index) >= end) {
+                        deleteLastChar = false;
+                        sb.append(oldText, linesBreak.get(index), oldText.length());
+                        break;
+                    }
+
+                    if (linesBreak.get(index + 1) > start) {
+                        sb.append('\t');
+                        totalAdded++;
+                    }
+                    sb.append(oldText, linesBreak.get(index), linesBreak.get(index + 1));
+                    sb.append("\n");
+                }
+                if(deleteLastChar) sb.deleteCharAt(sb.length() - 1);
+                if (cursor == start) {
+                    cursor += 1;
+                    selectionStart += totalAdded;
+                } else {
+                    selectionStart += 1;
+                    cursor += totalAdded;
+                }
+
+                text = sb.toString();
+                scheduleKeyRepeatTask(keycode);
+                return true;
+            }
+            return super.keyDown(event, keycode);
+        }
+
+        @Override
+        public boolean keyTyped(InputEvent event, char character) {
+            if (character == TAB && hasSelection) {
+                updateDisplayText();
+                return true;
+            }
+            return super.keyTyped(event, character);
+        }
+
+        @Override
+        public boolean scrolled(InputEvent event, float x, float y, float amountX, float amountY) {
+            scroll((int) amountY);
+            return true;
+        }
     }
 
 }
